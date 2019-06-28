11d7b4b11be0031d4c42947fe11b1b7fb52355be
seema kumari <seemakumari.dz01@gmail.com>
2019-06-28 14:10:39 +0530

    recreate access token for git api

--- src/main/java/com/todo/gitapp/config/APIConfiguration.java
+++ src/main/java/com/todo/gitapp/config/APIConfiguration.java
@@ -1,7 +1,7 @@
 package com.todo.gitapp.config;
 
 public interface APIConfiguration {
-    static final String ACCESS_TOKEN = "0bddf2c0f235d870d2cf1301cba01587f8a71862";
+    static final String ACCESS_TOKEN = "b96cd6475abe1bbab4b8962a31481dd25f42cab3";
     static final String API_BASE_URL = "https://api.github.com/";
     static final String API_VERSION_SPEC = "application/vnd.github.v3+json";
     static final String JSON_CONTENT_TYPE = "application/json";
